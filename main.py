9110f070de2e1e553e5581947f32bb0b6ff642f0
Rafragon <rafapokegamer.sm@gmail.com>
2026-05-21 01:43:33 -0300

    adicionado uma sequencia de cadeias

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 import os
 
+
+set_debug(True)
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 
+class Destino(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar")
+    motivo: str = Field(
+        "O motivo pelo qual é interessante visitar essa cidade")
+
+
+class Restarantes(BaseModel):
+    cidade: str = Field("A cidade recomendada para visitar")
+    restaurantes: str = Field("Restaurantes recomendados na cidade")
+
+
+parseador_destino = JsonOutputParser(pydantic_object=Destino)
+parseador_restaurantes = JsonOutputParser(pydantic_object=Restarantes)
+
 prompt_cidade = PromptTemplate(
     template="""
     Sugira uma cidade dado o meu interesse por {interesse}.
+
+    {formato_de_saida}
     """,
-    input_variables=["interesse"]
+    input_variables=["interesse"],
+    partial_variables={
+        'formato_de_saida': parseador_destino.get_format_instructions()}
 )
+
+prompt_restaurantes = PromptTemplate(
+    template="""
+    Sugira restaurantes populares entre locais em {cidade}.
+
+    {formato_de_saida}
+    """,
+    partial_variables={
+        'formato_de_saida': parseador_restaurantes.get_format_instructions()}
+)
+
+prompt_cultural = PromptTemplate(
+    template="Sugira atividades e locais culturais em {cidade}"
+)
+
 
 """
 # Este modelo que deveria ser utilizado mas a API é paga.
@@ -31,7 +68,11 @@ modelo = ChatGoogleGenerativeAI(
     api_key=api_key
 )
 
-cadeia = prompt_cidade | modelo | StrOutputParser()
+cadeia_1 = prompt_cidade | modelo | parseador_destino
+cadeia_2 = prompt_restaurantes | modelo | parseador_restaurantes
+cadeia_3 = prompt_cultural | modelo | StrOutputParser()
+
+cadeia = (cadeia_1 | cadeia_2 | cadeia_3)
 
 resposta = cadeia.invoke(
     {
